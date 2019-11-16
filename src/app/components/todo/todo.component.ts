f0bf4e2e66fce71ff542374f4514e08cbe52242b
DevLugo <bl1ndcod3@gmail.com>
2019-11-16 04:41:47 -0500

    estructura base todo funcional con servicios

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../models/Todo'; 
 import { TodoService } from '../../services/todo.service'
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -18,11 +19,22 @@ export class TodoComponent implements OnInit {
   }
 
   deleteTodo(todo:Todo){
+    console.log(this.todos);
+    console.log(todo);
     // Remove from UI
     this.todos = this.todos.filter(t => t.id !== todo.id);
     
     // Remove fron server
     this.todoService.deleteTodo(todo).subscribe();
   }
+
+  addTodo(todo:Todo){
+    this.todoService.addTodo(todo).subscribe(todo => {
+      console.log(todo)
+      this.todos.push(todo)
+    });
+  }
+
+  
 
 }
