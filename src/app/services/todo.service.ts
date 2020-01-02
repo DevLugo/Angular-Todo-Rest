ddb4cbaf7b8171cc0a77ef5a1ee7bc5d296471b5
lugomulish <elugo.isi@gmail.com>
2020-01-01 20:25:13 -0500

    creamos router y vista de registro/login

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,41 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Todo } from '../models/Todo';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Todo } from "../models/Todo";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json"
   })
-}
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TodoService {
-  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
-  todosLimit:string = '?_limit=6'
-  constructor(private http:HttpClient) { }
+  todosUrl: string = environment.apiUrl;
+  todosLimit: string = "?_limit=6";
+  constructor(private http: HttpClient) {}
 
   // Get Todos
-  getTodos():Observable<Todo[]> {
+  getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 
   // Toggle Completed
-  toggleComplelted(todo:Todo):Observable<any>{
+  toggleComplelted(todo: Todo): Observable<any> {
     const url = `${this.todosUrl}/${todo.id}`;
-    return this.http.put(url, todo, httpOptions)
+    return this.http.put(url, todo, httpOptions);
   }
 
   // Delete Todo
-  deleteTodo(todo:Todo): Observable<Todo>{
+  deleteTodo(todo: Todo): Observable<Todo> {
     const url = `${this.todosUrl}/${todo.id}`;
     return this.http.delete<Todo>(url, httpOptions);
   }
 
   // Add Todo
-  addTodo(todo:Todo): Observable<Todo>{
+  addTodo(todo: Todo): Observable<Todo> {
     return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
   }
 }
